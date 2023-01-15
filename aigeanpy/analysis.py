7eec0d290f92d8e424319b71b14c1cc2ebb0c040
Weizhi666 <ucapuab@ucl.ac.uk>
2023-01-15 16:34:05 +0000

    complete the analysis.py, use refactored cluster

--- aigeanpy/analysis.py
+++ aigeanpy/analysis.py
@@ -1,7 +1,13 @@
-def kmeans(_parameters: 'Parameters_Data_Type') -> 'Returns_Data_Type':
-    '''
-    Docstring
-    '''
 
-    ...
-    raise NotImplementedError
+from pathlib import Path
+
+def kmeans(filename, clusters: int = 3, iterations: int = 10) -> list:
+    
+    import clustering
+
+    clustering.cluster(filename, clusters, iterations)
+
+    return clustering.cluster(filename, clusters, iterations)
+
+if __name__ == '__main__':
+    print(kmeans('samples1.csv'))

d60c9c872e59a9a1da646c6306535c791c94f621
Weizhi666 <ucapuab@ucl.ac.uk>
2023-01-15 16:25:14 +0000

    Replace repeated code with a function

--- aigeanpy/clustering.py
+++ aigeanpy/clustering.py
@@ -4,33 +4,51 @@ import sys
 
 groups = 3
 
-lines = open(sys.argv[1], 'r').readlines()
+#with safty open
+with open(sys.argv[1], 'r') as file:
+  lines = file.readlines()
 
 process_data=[]
 for line in lines: 
   process_data.append(tuple(map(float, line.strip().split(','))))# merge the data into list
 
-random_point=[process_data[randrange(len(process_data))], process_data[randrange(len(process_data))], process_data[randrange(len(process_data))]] # random find the piont in dataset
+central_point=[process_data[randrange(len(process_data))] for g in range(groups)] # random find the piont in dataset
+
+#[(),()]
+
+def distance2points(point1,Lpoint2):
+  Ldistance2point = []
+  for point in Lpoint2:
+    Ldistance2point.append(sqrt((point1[0]-point[0])**2 + (point1[1]-point[1])**2 + (point1[2]-point[2])**2))
+  return Ldistance2point
+
+def min_index(list):
+  return list.index(min(list))
+
+
+def group_datas(group,Lprocess_data,alloc_data):
+  group_data = []
+  for j, p in enumerate(Lprocess_data):
+    if alloc_data[j]==group:
+      group_data.append(p)
+  return group_data
+
 
 alloc=[]
 n=0
 while n<10:
   for point in process_data:
-    distance2point=[None] * 3
     #find each distance to the random point of each point
-    distance2point[0]=sqrt((point[0]-random_point[0][0])**2 + (point[1]-random_point[0][1])**2 + (point[2]-random_point[0][2])**2)
-    distance2point[1]=sqrt((point[0]-random_point[1][0])**2 + (point[1]-random_point[1][1])**2 + (point[2]-random_point[1][2])**2)
-    distance2point[2]=sqrt((point[0]-random_point[2][0])**2 + (point[1]-random_point[2][1])**2 + (point[2]-random_point[2][2])**2)
-    alloc += [distance2point.index(min(distance2point))]# find the min distance index
-    print(alloc)
-  for i in range(3):
-    alloc_process_data=[point_ for index, point_ in enumerate(process_data) if alloc[index] == i]
+    Ldistance = distance2points(point,central_point)
+    alloc.append(min_index(Ldistance))# find the min distance index
+  for g in range(groups):
+    alloc_process_data=group_datas(g,process_data,alloc)
     new_mean=(sum([a[0] for a in alloc_process_data]) / len(alloc_process_data), sum([a[1] for a in alloc_process_data]) / len(alloc_process_data), sum([a[2] for a in alloc_process_data]) / len(alloc_process_data))
-    random_point[i]=new_mean
+    central_point[g]=new_mean
   n=n+1
 
-for i in range(3):
-  alloc_process_data=[point_ for index, point_ in enumerate(process_data) if alloc[index] == i]
-  print("Cluster " + str(i) + " is centred at " + str(random_point[i]) + " and has " + str(len(alloc_process_data)) + " points.")
+for g in range(groups):
+  alloc_process_data = group_datas(g,process_data,alloc)
+  print("Cluster " + str(g) + " is centred at " + str(central_point[g]) + " and has " + str(len(alloc_process_data)) + " points.")
 
   print(alloc_process_data)

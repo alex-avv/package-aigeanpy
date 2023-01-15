fdde177563efba23acb0d99af39a4260600cd0d1
Weizhi666 <ucapuab@ucl.ac.uk>
2023-01-15 16:38:27 +0000

    add clustering method in analysis.py

--- aigeanpy/analysis.py
+++ aigeanpy/analysis.py
@@ -1,13 +1,17 @@
 
 from pathlib import Path
 
-def kmeans(filename, clusters: int = 3, iterations: int = 10) -> list:
+def kmeans(filename, clusters: int = 3, iterations: int = 10, isNumpy = False) -> list:
     
-    import clustering
+    if isNumpy == False:
+        import clustering
 
-    clustering.cluster(filename, clusters, iterations)
+        return clustering.cluster(filename, clusters, iterations)
+    
+    if isNumpy == True:
+        import clustering_numpy
 
-    return clustering.cluster(filename, clusters, iterations)
+        return clustering_numpy.cluster(filename, clusters, iterations)
 
 if __name__ == '__main__':
     print(kmeans('samples1.csv'))

99120932eda4a0e82282e7d309d40b505d43d9c1
Weizhi666 <ucapuab@ucl.ac.uk>
2023-01-15 16:23:12 +0000

    Change of variable name

--- aigeanpy/clustering.py
+++ aigeanpy/clustering.py
@@ -2,36 +2,35 @@ from math import *
 from random import *
 import sys
 
-k=3
+iteration =3
 
 lines = open(sys.argv[1], 'r').readlines()
 
+process_data=[]
+for line in lines: 
+  process_data.append(tuple(map(float, line.strip().split(','))))# merge the data into list
 
+random_point=[process_data[randrange(len(process_data))], process_data[randrange(len(process_data))], process_data[randrange(len(process_data))]] # random find the piont in dataset
 
-
-ps=[]
-for line in lines: ps.append(tuple(map(float, line.strip().split(','))))
-
-m=[ps[randrange(len(ps))], ps[randrange(len(ps))], ps[randrange(len(ps))]]
-
-alloc=[None]*len(ps)
+alloc=[None]*len(process_data)
 n=0
 while n<10:
-  for i in range(len(ps)):
-    p=ps[i]
-    d=[None] * 3
-    d[0]=sqrt((p[0]-m[0][0])**2 + (p[1]-m[0][1])**2 + (p[2]-m[0][2])**2)
-    d[1]=sqrt((p[0]-m[1][0])**2 + (p[1]-m[1][1])**2 + (p[2]-m[1][2])**2)
-    d[2]=sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2 + (p[2]-m[2][2])**2)
-    alloc[i]=d.index(min(d))
+  for i in range(len(process_data)):
+    point=process_data[i]
+    distance2point=[None] * 3
+    #find each distance to the random point of each point
+    distance2point[0]=sqrt((point[0]-random_point[0][0])**2 + (point[1]-random_point[0][1])**2 + (point[2]-random_point[0][2])**2)
+    distance2point[1]=sqrt((point[0]-random_point[1][0])**2 + (point[1]-random_point[1][1])**2 + (point[2]-random_point[1][2])**2)
+    distance2point[2]=sqrt((point[0]-random_point[2][0])**2 + (point[1]-random_point[2][1])**2 + (point[2]-random_point[2][2])**2)
+    alloc[i]=distance2point.index(min(distance2point))# find the min distance index
   for i in range(3):
-    alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
-    new_mean=(sum([a[0] for a in alloc_ps]) / len(alloc_ps), sum([a[1] for a in alloc_ps]) / len(alloc_ps), sum([a[2] for a in alloc_ps]) / len(alloc_ps))
-    m[i]=new_mean
+    alloc_process_data=[point_ for index, point_ in enumerate(process_data) if alloc[index] == i]
+    new_mean=(sum([a[0] for a in alloc_process_data]) / len(alloc_process_data), sum([a[1] for a in alloc_process_data]) / len(alloc_process_data), sum([a[2] for a in alloc_process_data]) / len(alloc_process_data))
+    random_point[i]=new_mean
   n=n+1
 
 for i in range(3):
-  alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
-  print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")
+  alloc_process_data=[point_ for index, point_ in enumerate(process_data) if alloc[index] == i]
+  print("Cluster " + str(i) + " is centred at " + str(random_point[i]) + " and has " + str(len(alloc_process_data)) + " points.")
 
-  print(alloc_ps)
+  print(alloc_process_data)

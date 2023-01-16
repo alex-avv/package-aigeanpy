7b17c6036727068a399490bc90c1b10afd5b3852
Alejandro Avila Carrion <thealexabi@gmail.com>
2023-01-16 18:52:08 +0000

    feat: Added function to print metadata information

--- aigeanpy/command.py
+++ aigeanpy/command.py
@@ -1,7 +1,49 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from numpy import inf
 import sys
+from pathlib import Path; from os import getcwd; CWD = Path(getcwd())
 from aigeanpy.satmap import get_satmap
+
+
+def output_info(files, get_satmap, DIR):
+    failed = ''
+    missing = ''
+    
+    for file in files:
+        # Try processing the file, if not store the missing/failed file name
+        try:
+            meta = get_satmap(file).meta
+        except:
+            # Checking whether file exits
+            file_path = DIR/file
+            if not file_path.is_file():
+                missing += f' - {file}\n'
+            else:
+                failed += f' - {file}\n'
+            meta = None
+        
+        if meta:
+            # Replacing empty data with informative message
+            for key in meta:
+                if meta[f'{key}'] == '':
+                    meta[f'{key}'] = '<No information available>'
+            
+            # Printing screen information
+            if len(files) == 1:
+                for key in meta:
+                    value = meta[f'{key}']
+                    sys.stdout.write(f'{key}: {value}\n')
+            else:
+                for key in meta:
+                    value = meta[f'{key}']
+                    sys.stdout.write(f'{file}:{key}: {value}\n')
+    
+    # Print messages with missing/failed files
+    if failed:
+        sys.stdout.write("\nThese files failed while being processed\n"
+                         f"{failed}")
+    if missing:
+        sys.stdout.write(f"\nThese files couldn't be found\n{missing}")
 
 
 def unordered_mosaic(satmaps, resolution = None):

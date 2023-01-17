77a3d644fdee2c499bdd56dd51cf35d65a711273
Alejandro Avila Carrion <thealexabi@gmail.com>
2023-01-17 00:12:15 +0000

    ci: Removed Action's only main branch requirement

--- aigeanpy/tests/test_command.py
+++ aigeanpy/tests/test_command.py
@@ -22,7 +22,7 @@ with open(TEST_DIR/'fixtures/fixture_command.yml', 'r',
 # ~~~ AIGEAN_TODAY TESTS ~~~
 # Mocking aigeanpy.net.query_isa function requiring internet connection.
 # With Aigean observations from the 13/Jan/2023
-def query_isa(instrument = ''):
+def query_isa(instrument=''):
     with open(TEST_DIR/'extra_aigean_files/query_sample.json', 'r',
               encoding="utf-8") as json_query:
         list_query = json.load(json_query)
@@ -42,7 +42,7 @@ def query_isa(instrument = ''):
     return query
 
 
-@mark.parametrize('test_name', fixtures['today_arguments']) 
+@mark.parametrize('test_name', fixtures['today_arguments'])
 def test_today_arguments(test_name):
     properties = list(test_name.values())[0]
     parameters = properties['parameters']
@@ -58,20 +58,20 @@ def test_get_latest_obs(test_name):
     properties = list(test_name.values())[0]
     parameters = properties['parameters']
     expected_filename = properties['expected_value']
-    
+
     latest_obs = get_latest_obs(query_isa(parameters))
     assert latest_obs['filename'] == expected_filename
 
 
 # ~~~ AIGEAN_METADATA TESTS ~~~
-# Mocking aigeanpy.satmap.get_satmap function which requires to open files. 
+# Mocking aigeanpy.satmap.get_satmap function which requires to open files.
 # Returns 'SatMap' object with only-defined meta member variable.
 # With Aigean observations from the 05/Dec/2022 to the 20/Dec/2022
 def get_satmap_mock(filename):
     with open(TEST_DIR/'extra_aigean_files/metadata_sample.json', 'r',
               encoding="utf-8") as json_metadata:
         dict_metadata = json.load(json_metadata)
-    
+
     meta = dict_metadata[f'{filename}']
     # Changing the x- and y-coordinates (stored in the JSON file as lists) to
     # tuples
@@ -82,7 +82,7 @@ def get_satmap_mock(filename):
         if f'{key}' == 'xcoords':
             meta['xcoords'] = tuple(meta['xcoords'])
         elif f'{key}' == 'ycoords':
-            meta['ycoords'] = tuple(meta['ycoords'])   
+            meta['ycoords'] = tuple(meta['ycoords'])
 
     # Defining mock class
     class MockClass:  # pylint: disable = C0115, R0903
@@ -112,16 +112,16 @@ def test_metadata(capsys, test_name):
     properties = list(test_name.values())[0]
     parameters = properties['parameters']
     expected_output = properties['expected_value']
-    
+
     # Capturing expected print-out message
     sys.stdout.write(expected_output)
     expected_print = capsys.readouterr().out
 
     # Asserting print-out message when calling output_info is as expected
     os.chdir(TEST_DIR)
-    output_print = check_output(parameters).decode("utf-8").replace('\r','')
+    output_print = check_output(parameters).decode("utf-8").replace('\r', '')
     os.chdir(CWD)
-    assert output_print == expected_print   
+    assert output_print == expected_print
 
 
 # ~~~ AIGEAN_MOSAIC TESTS ~~~
@@ -136,7 +136,7 @@ def test_unordered_mosaic():
     fan_map_1 = get_satmap('aigean_fan_20230104_150010.zip')
     fan_map_2 = get_satmap('aigean_fan_20230112_074702.zip')
     fan_map_2.meta['obs_date'] = '2023-01-04 14:53:10'
-    
+
     satmaps = [man_map_1, man_map_2, fan_map_1, fan_map_2, lir_map_1,
                lir_map_2]
     # Trying to build mosaic in the specified ordered. Note that the correct
@@ -145,7 +145,7 @@ def test_unordered_mosaic():
     # will be added to the mosaic last.
     mosaic = unordered_mosaic(satmaps)
     # mosaic.visualise()
-    
+
     expected_array = np.load(TEST_DIR/'extra_aigean_files/'
                              'mosaic_data_sample.npy')
     np.testing.assert_almost_equal(mosaic.data, expected_array)
